9a532b56b73e307138edb02222adcf847bf48e9e
DangBaLoc <dangloc522@gmail.com>
2023-06-09 13:39:15 +0700

    exercise typescript

---
+++ fibonacci.js
@@ -0,0 +1,13 @@
+function fibonacci(num) {
+    var a = 1;
+    var b = 0;
+    var temp;
+    while (num > 0) {
+        temp = a;
+        a = a + b;
+        b = temp;
+        num--;
+    }
+    return b;
+}
+console.log(fibonacci(100));
---
+++ rectangle.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Rectangle = void 0;
+const shape_1 = require("./shape");
+class Rectangle extends shape_1.Shape {
+    constructor(x, y, _width, _height) {
+        super(x, y);
+        this._width = _width;
+        this._height = _height;
+    }
+    area() {
+        return this._width * this._height;
+    }
+}
+exports.Rectangle = Rectangle;
